3346a1cde584bd1d6b6d415323f97e873b546417
hayukleung <hayukleung@gmail.com>
2021-09-09 15:55:38 +0800

    demo - jetpack - lifecycle - fragment

--- products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/lifecycle/LifecycleActivity.kt
+++ products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/lifecycle/LifecycleActivity.kt
@@ -2,8 +2,13 @@ package com.af.demo.jetpack.lifecycle
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
 import android.widget.FrameLayout
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentTransaction
+import com.af.demo.jetpack.R
 
 /**
  * Created by hayukleung@gmail.com on 2021-09-09.
@@ -12,7 +17,12 @@ class LifecycleActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(FrameLayout(this))
+        setContentView(R.layout.activity_lifecycle)
+        findViewById<AppCompatButton>(R.id.button_show_fragment).setOnClickListener {
+            val fragmentTransaction = supportFragmentManager.beginTransaction()
+            fragmentTransaction.replace(R.id.fragment_holder, LifecycleFragment())
+            fragmentTransaction.commit()
+        }
         lifecycle.addObserver(ActivityObserver())
     }
 }
---
+++ products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/lifecycle/LifecycleFragment.kt
@@ -0,0 +1,37 @@
+package com.af.demo.jetpack.lifecycle
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.widget.AppCompatButton
+import androidx.fragment.app.Fragment
+import com.af.demo.jetpack.R
+
+/**
+ * Created by hayukleung@gmail.com on 2021-09-09.
+ */
+class LifecycleFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        lifecycle.addObserver(FragmentObserver())
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_lifecycle, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+
+        view.findViewById<AppCompatButton>(R.id.button_hide_fragment).setOnClickListener {
+
+           parentFragmentManager.beginTransaction().remove(this).commit()
+        }
+    }
+}
