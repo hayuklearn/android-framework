c4ff29377f4ce43f14c3507b6aa0ba9c13a7b544
hayukleung <hayukleung@gmail.com>
2021-09-09 18:12:08 +0800

    demo - jetpack - navigation

---
+++ products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/navigation/NavigationActivity.kt
@@ -0,0 +1,17 @@
+package com.af.demo.jetpack.navigation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.af.demo.jetpack.R
+
+/**
+ * Created by hayukleung@gmail.com on 2021-09-09.
+ */
+class NavigationActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        setContentView(R.layout.activity_navigation)
+    }
+}
