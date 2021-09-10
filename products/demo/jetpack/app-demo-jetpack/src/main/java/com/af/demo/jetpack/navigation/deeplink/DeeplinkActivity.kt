772ed594730c9442b4272399c0012b8f07c6164d
hayukleung <hayukleung@gmail.com>
2021-09-10 14:03:16 +0800

    demo - jetpack - navigation - deeplink

---
+++ products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/navigation/deeplink/DeeplinkActivity.kt
@@ -0,0 +1,18 @@
+package com.af.demo.jetpack.navigation.deeplink
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.af.demo.jetpack.R
+
+/**
+ * Test For DeepLink
+ *
+ * Created by hayukleung@gmail.com on 2021-09-10.
+ */
+class DeeplinkActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_deeplink)
+    }
+}
