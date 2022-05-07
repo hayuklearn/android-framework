4d0a3a78bf643343b4328fe6d0226fb1158bd245
hayukleung <hayukleung@gmail.com>
2022-05-07 11:34:58 +0800

    配置修改

--- products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/navigation/main/BottomNavigationViewActivity.kt
+++ products/demo/jetpack/app-demo-jetpack/src/main/java/com/af/demo/jetpack/navigation/main/BottomNavigationViewActivity.kt
@@ -19,8 +19,10 @@ import android.view.WindowManager
 import android.os.Build
 import android.view.View
 import android.view.Window
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
 import com.af.demo.jetpack.R
-
+import com.af.demo.jetpack.databinding.ActivityBottomNavigationViewBinding
 
 /**
  * Activity Using BottomNavigationView & NavigationUI
@@ -29,54 +31,64 @@ import com.af.demo.jetpack.R
  */
 class BottomNavigationViewActivity : AppCompatActivity() {
 
+    private val binding by lazy { ActivityBottomNavigationViewBinding.inflate(layoutInflater) }
+
     @SuppressLint("ObsoleteSdkInt")
     override fun onCreate(savedInstanceState: Bundle?) {
 
         // set fullscreen --------------------------------------------------------------------------
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
-            window.setFlags(
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT,
-                WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
-            )
-        } else {
-            window.setFlags(
-                WindowManager.LayoutParams.FLAG_FULLSCREEN,
-                WindowManager.LayoutParams.FLAG_FULLSCREEN
-            )
-        }
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            val window: Window = window
-            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
-            window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            window.statusBarColor = Color.TRANSPARENT
-            window.navigationBarColor = Color.BLACK
-        }
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+        //     window.setFlags(
+        //         WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT,
+        //         WindowManager.LayoutParams.LAYOUT_IN_DISPLAY_CUTOUT_MODE_DEFAULT
+        //     )
+        // } else {
+        //     window.setFlags(
+        //         WindowManager.LayoutParams.FLAG_FULLSCREEN,
+        //         WindowManager.LayoutParams.FLAG_FULLSCREEN
+        //     )
+        // }
+        // if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+        //     val window: Window = window
+        //     window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS or WindowManager.LayoutParams.FLAG_TRANSLUCENT_NAVIGATION)
+        //     window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION or View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+        //     window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+        //     window.statusBarColor = Color.TRANSPARENT
+        //     window.navigationBarColor = Color.BLACK
+        // }
         // set fullscreen --------------------------------------------------------------------------
 
         super.onCreate(savedInstanceState)
 
-        TranslucentStatusCompat.requestTranslucentStatus(this)
-        setContentView(R.layout.activity_bottom_navigation_view)
+        // TranslucentStatusCompat.requestTranslucentStatus(this)
+        setContentView(binding.root)
 
-        val toolbar = findViewById<Toolbar>(R.id.toolbar)
-        toolbar.layoutParams.height = Android.getToolbarHeight(this) + Android.getStatusBarHeight(this)
-        toolbar.requestLayout()
+        val controller = ViewCompat.getWindowInsetsController(binding.root)
+        // 显示状态栏
+        controller?.show(WindowInsetsCompat.Type.statusBars())
+        // 隐藏状态栏
+        // controller?.hide(WindowInsetsCompat.Type.statusBars())
+
+        // 状态栏文字颜色改为黑色
+        controller?.isAppearanceLightStatusBars = true
+
+        // 状态栏文字颜色改为白色
+        // controller?.isAppearanceLightStatusBars = false
+
+        binding.toolbar.layoutParams.height = Android.getToolbarHeight(this) + Android.getStatusBarHeight(this)
+        binding.toolbar.requestLayout()
 
         // step 1
-        val host: NavHostFragment =
-            supportFragmentManager.findFragmentById(R.id.main_fragment_navigation_host) as NavHostFragment
+        val host: NavHostFragment = supportFragmentManager.findFragmentById(R.id.main_fragment_navigation_host) as NavHostFragment
         val navController = host.navController
-        // step 2
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation_view)
         // step 3
-        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+        NavigationUI.setupWithNavController(binding.bottomNavigationView, navController)
 
         Android.signature(this, packageName)
-
     }
 
     override fun onSupportNavigateUp(): Boolean {
+
         return findNavController(this, R.id.bottom_navigation_view).navigateUp()
     }
 }
